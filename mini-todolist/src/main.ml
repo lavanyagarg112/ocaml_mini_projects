5b8801aaf395a945a2c231a30069f7c0f82b792b
lavanyagarg112 <lavanyagarg@u.nus.edu>
2024-09-04 10:09:49 +0800

    made better the basic trying

--- mini-todolist/src/main.ml
+++ mini-todolist/src/main.ml
@@ -11,10 +11,10 @@ let complete_task todo = { todo with isCompleted = true }
 let show_task todo = 
   Printf.printf "Task: %s (Completed: %b) \n" todo.task todo.isCompleted
 
-let () = 
+let main () = 
   let task1 = create_task 1 "Buy milk" in
   let task2 = create_task 2 "Buy eggs" in
   show_task task1;
-  complete_task task2 |> show_task;
+  complete_task task2 |> show_task
 
-(* let () = main () *)
+let () = main ()
