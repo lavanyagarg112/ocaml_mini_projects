c72cd234c50749127fe1b42101610e7c75b75c4c
lavanyagarg112 <lavanyagarg@u.nus.edu>
2024-09-04 09:07:15 +0800

    basic testing of how todo could work

---
+++ mini-todolist/src/main.ml
@@ -0,0 +1,20 @@
+type todo = {
+  id: int;
+  task: string;
+  isCompleted: bool;
+}
+
+let create_task id task = { id; task; isCompleted = false }
+
+let complete_task todo = { todo with isCompleted = true }
+
+let show_task todo = 
+  Printf.printf "Task: %s (Completed: %b) \n" todo.task todo.isCompleted
+
+let () = 
+  let task1 = create_task 1 "Buy milk" in
+  let task2 = create_task 2 "Buy eggs" in
+  show_task task1;
+  complete_task task2 |> show_task;
+
+(* let () = main () *)
