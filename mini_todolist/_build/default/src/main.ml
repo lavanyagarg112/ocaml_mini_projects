75f007fe4315849e992e410b08d80acd711b010b
lavanyagarg112 <lavanyagarg@u.nus.edu>
2024-09-08 13:15:00 +0800

    priority added

--- mini_todolist/_build/default/src/main.ml
+++ mini_todolist/_build/default/src/main.ml
@@ -3,8 +3,8 @@ open Todo
 
 let main () =
   let todo_list = TodoList.empty in
-  let todo_list = TodoList.add_task todo_list (Todo.create_task 1 "Buy Milk") in
-  let todo_list = TodoList.add_task todo_list (Todo.create_task 2 "Buy eggs") in
+  let todo_list = TodoList.add_task todo_list (Todo.create_task 1 "Buy Milk" Medium) in
+  let todo_list = TodoList.add_task todo_list (Todo.create_task 2 "Buy eggs" High) in
 
   print_endline "All tasks: ";
   TodoList.show_all todo_list;
@@ -13,8 +13,13 @@ let main () =
   print_endline "All tasks after completing task 1: ";
   TodoList.show_all todo_list;
 
-  let todo_list = TodoList.remove_task todo_list 2 in
-  print_endline "All tasks after removing task 2: "; 
-  TodoList.show_all todo_list
+  (* let todo_list = TodoList.remove_task todo_list 2 in
+  print_endline "All tasks after removing task 2: ";
+  TodoList.show_all todo_list; *)
+
+  let sorted_todo_list = TodoList.sort_by_priority todo_list in
+  print_endline "All tasks sorted by priority: ";
+  TodoList.show_all sorted_todo_list
+
 
 let () = main ()
--- mini_todolist/_build/default/src/todo.ml
+++ mini_todolist/_build/default/src/todo.ml
@@ -1,15 +1,20 @@
+type priority = High | Medium | Low
 module Todo = struct 
+  
   type t = {
     id: int;
     task: string;
+    priority: priority;
     isCompleted: bool;
   }
 
-  let create_task id task = { id; task; isCompleted = false }
+  let create_task id task priority = { id; task; priority; isCompleted = false }
 
   let complete_task todo = { todo with isCompleted = true }
 
-  let show_task todo = 
-    Printf.printf "Task: %s (Completed: %b) \n" todo.task todo.isCompleted
+  let show_task todo =
+    Printf.printf "Task: %s (Priority: %s, Completed: %b)\n"
+      todo.task (match todo.priority with Low -> "Low" | Medium -> "Medium" | High -> "High")
+      todo.isCompleted
 
 end
